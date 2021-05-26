41b2c5c49b3befe18782a5d7aac1f9a7481384ea
kumokumo1653 <fjsda526hdbs@gmail.com>
2021-05-26 21:35:55 +0900

    first

---
+++ Typing/Assets/Script/test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Question q = new Question("あいうえお","あいうえお");
+
+    void Start()
+    {
+        string[][] str;
+        q.TransformWords(out str);
+        for(int i = 0; i < str.Length;i++){
+            string s = "";
+            for(int j = 0; j < str[i].Length;j++){
+                s += str[i][j] + ",";
+            }
+            Debug.Log(s);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
